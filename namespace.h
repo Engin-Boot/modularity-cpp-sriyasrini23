efc9ab17f9b2c65c4e9ad396fe5dd11bf83a70d3
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-08-30 12:28:44 +0400

    Update namespace.h

--- namespace.h
+++ namespace.h
@@ -41,5 +41,18 @@ namespace TelCoColorCoder
                 colorPairStr += MinorColorNames[minorColor];
                 return colorPairStr;
             }
+    
+        ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor =
+        (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+        (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+        }
+    
+        int TelCoColorCoder::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+        }
 
 }
