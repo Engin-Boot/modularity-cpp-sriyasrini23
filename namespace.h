d446b59c97a5e24e87564533f33cce21c5eca001
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-08-30 17:49:34 +0400

    Update namespace.h

--- namespace.h
+++ namespace.h
@@ -15,44 +15,4 @@ namespace TelCoColorCoder
     int numberOfMinorColors =
         sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-    class ColorPair {
-        private:
-            MajorColor majorColor;
-            MinorColor minorColor;
-        public:
-            ColorPair(MajorColor, MinorColor);
-            MajorColor getMajor();
-            MinorColor getMinor();
-            std::string ToString();
-    };
-
-          ColorPair::ColorPair(MajorColor major, MinorColor minor):
-          majorColor(major), minorColor(minor)
-          {}
-          MajorColor ColorPair::getMajor() {
-                return majorColor;
-          }
-          MinorColor ColorPair::getMinor() {
-                return minorColor;
-            }
-          std::string ColorPair::ToString() {
-                std::string colorPairStr = MajorColorNames[majorColor];
-                colorPairStr += " ";
-                colorPairStr += MinorColorNames[minorColor];
-                return colorPairStr;
-            }
-    
-        ColorPair GetColorFromPairNumber(int pairNumber) {
-        int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor =
-        (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor =
-        (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-        return ColorPair(majorColor, minorColor);
-        }
-    
-        int TelCoColorCoder::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * numberOfMinorColors + minor + 1;
-        }
-
 }
