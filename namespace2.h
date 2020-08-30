7356c42cdd02ac646f8dbfb8e4ac4e3af86b3fa1
sriyasrini23 <60133969+sriyasrini23@users.noreply.github.com>
2020-08-30 17:51:02 +0400

    Create namespace2.h

---
+++ namespace2.h
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace TelCoColorCoder
+{
+    class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor, MinorColor);
+            MajorColor getMajor();
+            MinorColor getMinor();
+            std::string ToString();
+    };
+
+          ColorPair::ColorPair(MajorColor major, MinorColor minor):
+          majorColor(major), minorColor(minor)
+          {}
+          MajorColor ColorPair::getMajor() {
+
+                    return majorColor;
+          }
+          MinorColor ColorPair::getMinor() {
+                return minorColor;
+            }
+          std::string ColorPair::ToString() {
+                std::string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
+    
+        ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor =
+        (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor =
+        (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+        }
+        int TelCoColorCoder::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+        }
+}
